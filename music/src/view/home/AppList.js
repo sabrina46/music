436f8a9f18dfb566ebe720d60baeca6e9b1fab7e
cjx <460740525@qq.com>
2019-08-27 02:28:03 +0800

    列表优化

--- music/src/view/home/AppList.js
+++ music/src/view/home/AppList.js
@@ -32,6 +32,10 @@ class AppList extends React.Component {
       keyword: ''
     };
   }
+  init() {
+    if (!this.state.keyword) this.getList();
+    else this.searchList();
+  }
   getList() {
     let pageIndex = this.state.pageIndex;
     let that = this;
@@ -50,7 +54,6 @@ class AppList extends React.Component {
             sort: i + 1 + (pageIndex - 1) * pageSize
           };
         });
-        console.log(pageIndex === 0 ? 0 : (pageIndex - 1) * pageSize);
 
         //这里表示上拉加载更多
         let rdata = [...that.state.appList, ...data];
@@ -63,39 +66,50 @@ class AppList extends React.Component {
       .catch(e => console.log('错误:', e));
   }
   searchList() {
-    if (!this.props.keyword) return;
+    let pageIndex = this.state.pageIndex;
     fetch('../data/lookUp.json', {
       method: 'get',
-      dataType: 'json',
-      body: { keyword: this.props.keyword }
+      dataType: 'json'
+      // body: JSON.stringify({ keyword: this.props.keyword })
     })
       .then(res => res.json())
       .then(res => {
-        let data = res.results.map(item => {
-          let category = item.genres.length > 1 ? item.genres.splice(0, 2).join('和') : item.genres[0];
+        // let data = res.results.map(item => {
+        //   let category = item.genres.length > 1 ? item.genres.splice(0, 2).join('和') : item.genres[0];
+        //   return {
+        //     img: item.screenshotUrls[0],
+        //     title: item.trackName,
+        //     category: category
+        //   };
+        // });
+        let data = res.results.slice(pageIndex === 0 ? 0 : (pageIndex - 1) * pageSize, pageIndex * pageSize).map((item, i) => {
           return {
-            img: item.screenshotUrls[0],
-            title: item.trackName,
-            category: category
+            img: item['im:image'][0]['label'],
+            title: item.title.label,
+            category: item.category.attributes.label,
+            rate: i,
+            sort: i + 1 + (pageIndex - 1) * pageSize
           };
         });
-        this.setState({ searchList: data });
-        // this.props.setKeyword(data)
+        let rdata = [...this.state.appList, ...data];
+        this.setState({
+          appList: rdata,
+          dataSource: this.state.dataSource.cloneWithRows(rdata),
+          isLoading: data.length !== 0
+        });
       })
-      .catch(e => this.clearList());
+      .catch(e => console.log('错误：', e));
   }
   componentDidMount() {
     this.getList(true);
     store.subscribe(() => {
-      let {
-        common: { age: iptVal }
-      } = store.getState();
-      this.setState({ iptVal });
+      console.log(store.getState());
+      this.setState({ appList: [], ...store.getState() }, this.init);
     });
   }
   onRefresh = () => {
     let that = this;
-    this.setState({ refreshing: true, isLoading: true, pageIndex: 1 });
+    this.setState({ isLoading: true, pageIndex: 1 });
     setTimeout(() => {
       that.getList(true);
     }, 2000);
@@ -111,7 +125,6 @@ class AppList extends React.Component {
     }, 1000);
   };
   render() {
-    console.log(this.props.keyword, 'keyword');
     let data = this.state.appList;
     let index = data.length - 5;
     const row = (rowData, sectionID, rowID) => {
